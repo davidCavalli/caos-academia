30f5c3ee16b0c4b8a248f31697cc4a483b1e5a48
davidCavalli <dvdcavalli@gmail.com>
2025-09-24 05:07:53 -0400

    Add initial implementation of Academia class with weight management methods

---
+++ academia.py
@@ -0,0 +1,37 @@
+import random
+
+
+class Academia:
+    def __init__(self):
+        self.halteres = [i for i in range(10, 36) if i % 2 == 0]
+        self.porta_halteres = {}
+        self.reiniciar_o_dia()
+
+    def reiniciar_o_dia(self):
+        self.porta_halteres = {i: i for i in self.halteres}
+    
+    def listar_halteres(self):
+        return [i for i in self.porta_halteres.values() if i != 0]
+    
+    def pegar_halteres(self, peso):
+        if peso in self.porta_halteres and self.porta_halteres[peso] != 0:
+            self.porta_halteres[peso] = 0
+            return peso
+        return None
+        # versão professor
+        halt_pos = list(self.porta_halteres.values()).index(peso)
+        key_halt = list(self.porta_halteres.keys())[halt_pos]
+        self.porta_halteres[key_halt] = 0
+        return peso
+    
+    def devolver_halter(self, pos, peso):
+        if pos in self.porta_halteres and self.porta_halteres[pos] == 0:
+            self.porta_halteres[pos] = peso
+            return True
+        return False
+        # versão professor
+        self.porta_halteres[pos] = peso
+
+    def calcular_caos(self):
+        num_caos = [i for i, j in self.porta_halteres.items() if i != j]
+        return len(num_caos) / len(self.halteres) * 100
